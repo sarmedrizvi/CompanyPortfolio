e7ed5675a2978df532c915c4174078036cc1b56c
sarmedrizvi <sarmedrizvi@gmail.com>
2019-10-24 06:42:31 +0500

    Updates

---
+++ precache-manifest.c7bd7ac9fe53814e00d32ca40b26e1b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdf5472d28d7582f73dd96f1af0ebec",
+    "url": "/CompanyPortfolio/index.html"
+  },
+  {
+    "revision": "e7dbc4d251429f1d7c6d",
+    "url": "/CompanyPortfolio/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "02d6d343937bddeb0acc",
+    "url": "/CompanyPortfolio/static/css/main.72023074.chunk.css"
+  },
+  {
+    "revision": "e7dbc4d251429f1d7c6d",
+    "url": "/CompanyPortfolio/static/js/2.56a54923.chunk.js"
+  },
+  {
+    "revision": "02d6d343937bddeb0acc",
+    "url": "/CompanyPortfolio/static/js/main.1b603b1a.chunk.js"
+  },
+  {
+    "revision": "9af08624872834b9b7bb",
+    "url": "/CompanyPortfolio/static/js/runtime-main.41495311.js"
+  },
+  {
+    "revision": "f71bfc5fd75fdb0a2350bb4be06c7db2",
+    "url": "/CompanyPortfolio/static/media/main-background.f71bfc5f.jpg"
+  },
+  {
+    "revision": "2a1f375cb3c7e81ded4361d0d72fe8aa",
+    "url": "/CompanyPortfolio/static/media/softlamp-image.2a1f375c.jpg"
+  }
+]);
