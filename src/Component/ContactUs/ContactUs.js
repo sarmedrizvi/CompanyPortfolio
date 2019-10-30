e8cf13f917005524ff45b9916a351a35e173b5e5
sarmedrizvi <sarmedrizvi@gmail.com>
2019-10-30 18:11:30 +0500

    Contact Us Modified

--- src/Component/ContactUs/ContactUs.js
+++ src/Component/ContactUs/ContactUs.js
@@ -1,36 +1,43 @@
 import React from 'react'
 import './ContactUs.scss'
-import {InputButton} from '../Input-Button/Button'
-const Logo=({logoClass,subtitles})=>(
+import { InputButton } from '../Input-Button/Button'
+const Logo = ({ logoClass, subtitles }) => (
     <div className='logo'>
         <span className={`fa fa-${logoClass} logo1`}></span>
         <span className='logo-subtitle'>{subtitles}</span>
     </div>
 )
-const ContactForm=()=>(
+
+const ContactForm = () => (
     <div className='contactform'>
         <h2 className='contactform-title'>Contact Form</h2>
-        <label>first Name</label>
-        <input type='text'></input>
-        <label>Last Name</label>
-        <input type='text'></input>
-        <label>Email</label>
-        <input type='email'></input>
-        <label>Subject</label>
-        <input type='text'></input>
-        <label>Message</label>
-        <input type='text'></input>
+        <div className='contactform-two'>
+            <div className='contactform-two-comp'>
+                <label className='label shrink'>First Name</label>
+                <input type='text' className='textbox shrink' ></input>
+            </div>
+            <div className='contactform-two-comp'>
+                <label className='label shrink'>Last Name</label>
+                <input type='text' className='textbox shrink'></input>
+            </div>
+        </div>
+        <label className='label'>Email</label>
+        <input type='email' className='textbox'></input>
+        <label className='label'>Subject</label>
+        <input type='text' className='textbox'></input>
+        <label className='label'>Message</label>
+        <input type='text' className='textbox message'></input>
         <InputButton type='submit'> Send Message</InputButton>
     </div>
 )
-export const ContactUs =()=>(
+export const ContactUs = () => (
     <div className='contactus'>
         <h1 className='contactus-title'>Contact Us</h1>
         <div className='contactus-logos'>
-            <Logo logoClass='map-marker' subtitles='Hello'/>
-            <Logo logoClass='phone' subtitles='Hello'/>
-            <Logo logoClass='envelope' subtitles='Hello'/>
+            <Logo logoClass='map-marker' subtitles='Hello' />
+            <Logo logoClass='phone' subtitles='Hello' />
+            <Logo logoClass='envelope' subtitles='Hello' />
         </div>
-        <ContactForm/>
+        <ContactForm />
     </div>
 )
