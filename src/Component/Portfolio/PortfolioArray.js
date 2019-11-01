042822132d28e11b1ef724ecc40c12e29f485042
sarmedrizvi <sarmedrizvi@gmail.com>
2019-11-01 20:52:55 +0500

    Some modification in portfolio

--- src/Component/Portfolio/PortfolioArray.js
+++ src/Component/Portfolio/PortfolioArray.js
@@ -3,42 +3,42 @@ export const PortfolioWeb=[
         image:'https://www.w3schools.com/w3css/img_lights.jpg'
     },
     {
-        image:'https://www.w3schools.com/w3css/img_lights.jpg'
+        image:'https://www.thelocal.fr/userdata/images/article/21ce9e6de95eb2d227451f186d0c6d54550a4364658b87c1c0326ba6127fff5f.jpg'
     },
     {
-        image:'https://www.w3schools.com/w3css/img_lights.jpg'
+        image:'https://www.w3schools.com/howto/img_nature_wide.jpg'
     },
     {
-        image:'https://www.w3schools.com/w3css/img_lights.jpg'
+        image:'https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg'
     }
 ]
 
 export const PortfolioMobile=[
     {
-        image:''
+        image:'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
     },
     {
-        image:''
+        image:'https://www.pond5.com/images/images_db/vlp/image-hero-poster.jpg'
     },
     {
-        image:''
+        image:'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg'
     },
     {
-        image:''
+        image:'https://media.istockphoto.com/photos/fairy-tree-in-mystic-forest-picture-id502735520?k=6&m=502735520&s=612x612&w=0&h=kMaZWd25caV3bKU3kHnFhRoRXhcxjiChw6nhYCTLktI='
     }
 ]
 
 export const PortfolioDesktop=[
     {
-        image:''
+        image:'https://timesofindia.indiatimes.com/thumb/msid-66476517,imgsize-196276,width-800,height-600,resizemode-4/66476517.jpg'
     },
     {
-        image:''
+        image:'https://media.istockphoto.com/photos/abstract-geometric-landscape-picture-id832632796?k=6&m=832632796&s=612x612&w=0&h=fA_-At_kccr38c3MTh_029UbGGpkHSVf-hCULZM3YDw='
     },
     {
-        image:''
+        image:'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     },
     {
-        image:''
+        image:'https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg'
     }
 ]
