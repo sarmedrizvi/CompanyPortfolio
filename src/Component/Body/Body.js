23c5b3be38826daa395f829e80fdb76c0af6dd5d
sarmedrizvi <sarmedrizvi@gmail.com>
2019-11-05 11:06:20 +0500

    Join Us button link added

--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { InputButton } from '../Input-Button/Button'
 import './Body.scss'
 
+const raiseInvoiceClicked=()=>(
+    // your axios call here
+    // localStorage.setItem("pageData", "Data Retrieved from axios request")
+    // route to new page by changing window.location
+    window.open("https://gmail.us3.list-manage.com/subscribe/post?u=c8f045533e38a546bd9b77982&amp;id=41ea4d2249", "_blank") //to open new page
+)
 
 export const Body = () => (
+    
     <div className='main-body' id='body'>
         <div className='main-item'>
             <h1 className='main-title'>“Any time is a good time to start a company.” -Ron Conway</h1>
-            <InputButton >Join Us</InputButton>
+            <InputButton OnClick={raiseInvoiceClicked} >Join Us</InputButton>
         </div>
     </div>
+
 )
--- src/Component/Input-Button/Button.js
+++ src/Component/Input-Button/Button.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Button.scss'
 
 
-export const InputButton=({children,isGoogleButton,...otherProps})=>(
+export const InputButton=({children,isGoogleButton,OnClick,...otherProps})=>(
 
     <button
     className={`${isGoogleButton? 'GoogleButton' : ''} inputButton`}
     {...otherProps}
+    onClick={OnClick}
     >
     {children}
     </button>
